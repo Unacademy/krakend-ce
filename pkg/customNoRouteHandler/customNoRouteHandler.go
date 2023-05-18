ec80c446b8ff39840fd68d49d4f6c94a226cf175
Tarun Duhan <tarunduhan@ip-192-168-1-149.ap-southeast-1.compute.internal>
2023-05-18 13:39:26 +0530

    adding customNoRouteHandler package

---
+++ pkg/customNoRouteHandler/customNoRouteHandler.go
@@ -0,0 +1,78 @@
+package customnoroutehandler
+
+import (
+	"io"
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+	"github.com/luraproject/lura/logging"
+)
+
+type NoRouteHandler struct {
+	httpClient    *http.Client
+	defaultURL    string
+	defaultScheme string
+	logger        logging.Logger
+}
+
+func NewNoRouteHandler(defaultURL, defaultScheme string, logger logging.Logger) *NoRouteHandler {
+	h := &NoRouteHandler{
+		defaultURL:    defaultURL,
+		defaultScheme: defaultScheme,
+		logger:        logger,
+	}
+
+	h.InitialiseHttpClient()
+
+	return h
+}
+
+func (h *NoRouteHandler) InitialiseHttpClient() {
+	transport := &http.Transport{
+		MaxIdleConns:        100,
+		MaxIdleConnsPerHost: 100,
+	}
+
+	h.httpClient = &http.Client{
+		Transport: transport,
+	}
+}
+
+func (h *NoRouteHandler) GetClient() *http.Client {
+	return h.httpClient
+}
+
+func (h *NoRouteHandler) ForwardRequestToDefaultURL(c *gin.Context) {
+	client := h.GetClient()
+	req, err := http.NewRequest(c.Request.Method, c.Request.URL.String(), c.Request.Body)
+	if err != nil {
+		h.logger.Error("Failed to create request:", err.Error())
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to create request"})
+		return
+	}
+
+	req.Header = c.Request.Header
+	req.URL.Scheme = h.defaultScheme
+	req.URL.Host = h.defaultURL
+
+	resp, err := client.Do(req)
+	if err != nil {
+		h.logger.Error("Failed to forward request:", err.Error())
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to forward request"})
+		return
+	}
+
+	for key, values := range resp.Header {
+		for _, value := range values {
+			c.Writer.Header().Add(key, value)
+		}
+	}
+
+	c.Status(resp.StatusCode)
+	_, copyErr := io.Copy(c.Writer, resp.Body)
+	if err != nil {
+		h.logger.Error("Failed to copy response:", copyErr.Error())
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "Failed to copy response"})
+		return
+	}
+}
